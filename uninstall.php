9bcffae0f2f2ea65808617ff74376080f0ec5764
feeling4design <info@feeling4design.nl>
2016-02-18 01:20:09 +0100

    No longer delete settings when deleting/uninstalling plugin

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,12 @@
 <?php
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) 
 	exit();
-	
-delete_option('super_settings');
+
+/**
+ *	@since		1.0.8.3
+ *
+ * We do no longer want to delete the Super Forms settings because
+ * this will cause trouble when users forget to export their settings
+ *
+*/
+//delete_option('super_settings');
