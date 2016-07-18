16eab9f1cc27fb5dfc51821ca7017053a9a0a4a8
feeling4design <info@feeling4design.nl>
2016-07-18 23:20:29 +0200

    Import entries changes

--- includes/class-ajax.php
+++ includes/class-ajax.php
@@ -48,6 +48,7 @@ class SUPER_Ajax {
             'import_settings'               => false,
             'export_entries'                => false, // @since 1.1.9
             'prepare_contact_entry_import'  => false, // @since 1.2.6
+            'import_contact_entries'        => false, // @since 1.2.6
 
 
         );
@@ -301,7 +302,48 @@ class SUPER_Ajax {
     /** 
      *  Import Contact Entries (from CSV file)
      *
-     *  @since      1.1.9
+     *  @since      1.2.6
+    */
+    public static function import_contact_entries() {
+        $file_id = absint( $_REQUEST['file_id'] );
+        $column_settings = $_REQUEST['column_connections'];
+        var_dump($file_id);
+        var_dump($column_connections);
+
+        /*
+        $delimiter = ',';
+        $enclosure = '"';
+        $file = get_attached_file($file_id);
+        $columns = array();
+        if( $file ) {
+            $row = 1;
+            if (($handle = fopen($file, "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 0, $delimiter, $enclosure)) !== FALSE) {
+                    $num = count($data);
+                    $row++;
+                    $value = 'undefined';
+                    $title = 'undefined';
+                    for ( $c=0; $c < $num; $c++ ) {
+                        $columns[] = $data[$c];
+                    }
+                    break;
+                }
+                fclose($handle);
+            }
+        }
+        echo json_encode($columns);
+        */
+        die();
+
+
+
+    }
+
+
+    /** 
+     *  Prepare Contact Entries Import (from CSV file)
+     *
+     *  @since      1.2.6
     */
     public static function prepare_contact_entry_import() {
         $file_id = absint( $_REQUEST['file_id'] );
