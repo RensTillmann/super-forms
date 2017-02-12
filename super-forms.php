b692fed02640cb6e548f09977b46c7379f9b33e6
feeling4design <info@feeling4design.nl>
2017-02-12 14:06:59 +0100

    - Fix: Issue replacing tags when using a custom redirect after form submission

--- super-forms.php
+++ super-forms.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - Drag & Drop Form Builder
  * Plugin URI:  http://codecanyon.net/user/feeling4design
  * Description: Build forms anywhere on your website with ease.
- * Version:     2.5.3
+ * Version:     2.5.4
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
 */
@@ -36,7 +36,7 @@ if(!class_exists('SUPER_Forms')) :
          *
          *	@since		1.0.0
         */
-        public $version = '2.5.3';
+        public $version = '2.5.4';
 
 
         /**
