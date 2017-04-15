5648d9457775cc76402820b0bc8dd5d5ba3a5934
feeling4design <info@feeling4design.nl>
2017-04-15 22:06:45 +0200

    v2.9.0

--- includes/admin/views/page-create-form.php
+++ includes/admin/views/page-create-form.php
@@ -1,4 +1,141 @@
 <div class="super-create-form">
+
+    <?php
+    if( $post_ID==0 ) {
+        ?>
+        <div class="super-first-time-setup super-active">
+            <div class="super-wizard-settings">
+                <h2>Form setup wizard <input type="text" name="wizard_title" value="Form name" /></h2>
+                <ul class="super-tabs noselect">
+                    <li class="super-active">Theme & styles</li>
+                    <li>Admin email</li>
+                    <li>Confirmation email</li>
+                    <li>Thank you message</li>
+                </ul>
+                <ul class="super-tab-content">
+                    <li class="super-active">
+                        <div>
+                            <span>Theme style:</span>
+                            <ul class="super-theme-style-wizard noselect">
+                                <li class="super-active" data-value="squared">Squared</li>
+                                <li data-value="rounded">Rounded</li>
+                                <li data-value="full-rounded">Full Rounded</li>
+                                <li data-value="minimal">Minimal</li>
+                            </ul>
+                            <input type="hidden" name="wizard_theme_style" />
+                        </div>
+                        <div>
+                            <span>Field size:</span>
+                            <ul class="super-field-size-wizard noselect">
+                                <li class="super-active" data-value="medium">Medium</li>
+                                <li data-value="large">Large</li>
+                                <li data-value="huge">Huge</li>
+                            </ul>
+                            <input type="hidden" name="wizard_theme_field_size" />
+                        </div>
+                        <div>
+                            <span>Enable icons:</span>
+                            <ul class="super-theme-hide-icons-wizard noselect">
+                                <li class="super-active" data-value="no">No (default)</li>
+                                <li data-value="yes">Yes</li>
+                            </ul>
+                            <input type="hidden" name="wizard_theme_hide_icons" />
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <span>Send email to:</span>
+                            <input type="text" name="wizard_header_to" value="<?php echo get_option('admin_email'); ?>" />
+                        </div>
+                        <div>
+                            <span>From email:</span>
+                            <input type="text" name="wizard_header_from" value="<?php echo get_option('admin_email'); ?>" />
+                            <p>
+                                (if you encounter issues with receiving emails, try to use info@<strong style="color:red;"><?php echo $_SERVER["SERVER_NAME"]; ?></strong>)
+                                <?php
+                                $mail_error_msg = '<br /><span style="color:red;"><strong>Please note:</strong> mail() is disabled, setup SMTP to send emails.</span>';
+                                $mail_error = false;
+                                if ( !function_exists( 'mail' ) ) {
+                                    $mail_error = true;
+                                    echo $mail_error_msg;
+                                }else{
+                                    $to = "testmessageemail@testmessageemail.com";
+                                    $header = "From: {$to}";
+                                    $subject = "Hi!";
+                                    $body = "Hi,\n\nHow are you?";
+                                    if( !mail($to, $subject, $body, $header) ) {
+                                        $mail_error = true;
+                                        echo $mail_error_msg;
+                                    }
+                                }
+                                ?>
+                            </p>
+                        </div>
+                        <div>
+                            <span>From name:</span>
+                            <input type="text" name="wizard_header_from_name" value="<?php echo get_option('blogname'); ?>" />
+                        </div>
+                        <div>
+                            <span>Subject:</span>
+                            <input type="text" name="wizard_header_subject" value="<?php echo __( 'This mail was send from', 'super-forms' ) . ' ' . $_SERVER["SERVER_NAME"]; ?>" />
+                        </div>
+                        <div>
+                            <span>Body header:</span>
+                            <textarea name="wizard_email_body_open"><?php echo __( 'The following information has been send by the submitter:', 'super-forms' ); ?></textarea>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <span>Send email to:</span>
+                            <input type="text" name="wizard_confirm_to" value="{email}" />
+                            <p>(the tag {email} will automatically be replaced with the value of the field named <strong>email</strong>)</p>
+                        </div>
+                        <div>
+                            <span>From email:</span>
+                            <input type="text" name="wizard_confirm_from" value="<?php echo get_option('admin_email'); ?>" />
+                            <p>
+                                (if you encounter issues with receiving emails, try to use info@<strong style="color:red;"><?php echo $_SERVER["SERVER_NAME"]; ?></strong>)
+                                <?php if($mail_error) echo $mail_error_msg; ?>
+                            </p>
+                        </div>
+                        <div>
+                            <span>From name:</span>
+                            <input type="text" name="wizard_confirm_from_name" value="<?php echo get_option('blogname'); ?>" />
+                        </div>
+                        <div>
+                            <span>Subject:</span>
+                            <input type="text" name="wizard_confirm_subject" value="<?php echo get_option('blogname'); ?>" />
+                        </div>
+                        <div>
+                            <span>Body header:</span>
+                            <textarea name="wizard_confirm_body_open"><?php echo __( "Dear user,\n\nThank you for contacting us!", "super-forms" ); ?></textarea>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <span>Thank you title:</span>
+                            <input type="text" name="wizard_form_thanks_title" value="<?php echo __( 'Thank you!', 'super-forms' ); ?>" />
+                        </div>
+                        <div>
+                            <span>Thank you message:</span>
+                            <textarea name="wizard_form_thanks_description"><?php echo __( 'We will reply within 24 hours.', 'super-forms' ); ?></textarea>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="super-wizard-preview">
+                <h2>Form preview</h2>
+                <img data-preview-url="<?php echo SUPER_PLUGIN_FILE; ?>" src="<?php echo SUPER_PLUGIN_FILE . 'assets/images/wizard-preview/squared-medium.png'; ?>" />
+            </div>
+
+            <span class="super-button skip-wizard">Skip wizard</span>
+            <span class="super-button save-wizard">Save settings</span>
+        </div>
+        <div class="super-first-time-setup-bg super-active"></div>
+        <?php
+    }
+    ?>
+
     <div class="super-wrapper">
         <div class="super-header">
             <div class="super-switch-forms" >
@@ -43,12 +180,14 @@
             <?php
             // Try to load the selected theme style
             $id = $post_ID;
-            $theme_style = 'super-style-default ';
+            $theme_style = 'super-style-default';
             $style_content  = '';
             if( ( isset( $settings['theme_style'] ) ) && ( $settings['theme_style']!='' ) ) {
-                $theme_style = $theme_style . $settings['theme_style'];
+                $theme_style .= ' ' . $settings['theme_style'];
                 $style_content .= require_once( SUPER_PLUGIN_DIR . '/assets/css/frontend/themes/' . str_replace( 'super-', '', $settings['theme_style'] ) . '.php' );
             }
+            if( isset( $settings['theme_field_size'] ) ) $theme_style .= ' super-field-size-' . $settings['theme_field_size'];
+
             // Always load the default styles (these can be overwritten by the above loaded style file
             $style_content .= require_once( SUPER_PLUGIN_DIR . '/assets/css/frontend/themes/style-default.php' );
             ?>
