9b9900c7b0ea8122b61431e21d2e8f87456157ad
feeling4design <info@feeling4design.nl>
2017-11-22 15:56:20 +0100

    - Added: Option for Google Address Autocomplete to populate street name and number at once (combined)

--- includes/class-field-types.php
+++ includes/class-field-types.php
@@ -305,6 +305,7 @@ class SUPER_Field_Types {
         $mappings = array(
             'street_number' => __( 'Street number', 'super-forms' ),
             'street_name' => __( 'Street name', 'super-forms' ),
+            'street_name_number' => __( 'Street name + nr', 'super-forms' ),
             'city' => __( 'City name', 'super-forms' ),
             'state' => __( 'State/Province', 'super-forms' ),
             'postal_code' => __( 'Postal code', 'super-forms' ),
