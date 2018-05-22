564160b4cf27ddc2981b08cfdd17f1a977e7aca4
Rens <feeling4design@gmail.com>
2018-05-22 03:37:56 +0200

    sendinblue newsletter

--- super-forms.php
+++ super-forms.php
@@ -904,28 +904,8 @@ if(!class_exists('SUPER_Forms')) :
                         echo '<div class="super-whats-new">';
                             echo '<h3>Staying up to date:</h3>';
                             echo '<p>';
-                                echo 'To stay up to date with the latest news regarding Super Forms, <a target="_blank" href="https://codecanyon.net/user/feeling4design/followers">follow us on codecanyon</a> and subscribe to our newsletter:';
+                                echo 'To stay up to date with the latest news regarding Super Forms, <a target="_blank" href="https://codecanyon.net/user/feeling4design/followers">follow us on codecanyon</a> and <a target="_blank" href="https://my.sendinblue.com/users/subscribe/js_id/37455/id/1">subscribe to our newsletter</a>.';
                             echo '</p>';
-                            ?>
-                            <!-- Begin MailChimp Signup Form -->
-                            <div id="mc_embed_signup">
-                               <form action="https://feeling4design.us12.list-manage.com/subscribe/post?u=f5054b57a91eadb52326201bc&amp;id=f94d5728eb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                                  <div id="mc_embed_signup_scroll">
-                                     <div class="mc-field-group">
-                                        <input type="email" value="" placeholder="email address" name="EMAIL" class="required email" id="mce-EMAIL">
-                                     </div>
-                                     <div id="mce-responses" class="clear">
-                                        <div class="response" id="mce-error-response" style="display:none"></div>
-                                        <div class="response" id="mce-success-response" style="display:none"></div>
-                                     </div>
-                                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f5054b57a91eadb52326201bc_f94d5728eb" tabindex="-1" value=""></div>
-                                     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                                  </div>
-                               </form>
-                            </div>
-                            <!--End mc_embed_signup-->
-                            <?php
                         echo '</div>';
 
                         echo '<div class="super-whats-new">';
