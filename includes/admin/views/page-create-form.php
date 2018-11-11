36137c763df61bbea96868824131a7668b3f7f2e
Rens <feeling4design@gmail.com>
2018-11-11 17:07:57 +0100

    progress new ui

--- includes/admin/views/page-create-form.php
+++ includes/admin/views/page-create-form.php
@@ -22,23 +22,154 @@
 
         <div class="sf-items-wrapper">
             <div class="sf-items">
-                <?php
-                $i = 0;
-                while($i<10) {
-                    ?>
-                    <div class="sf-item sf-type-text">
-                        <div class="sf-title">
-                            <span>Text field</span>
-                        </div>
-                        <div class="sf-preview">
-                            <span class="sf-label">Dummy label</span>
-                            <input type="text" placeholder="Dummy placeholder..." />
+
+                <div class="sf-item sf-type-text">
+                    <div class="sf-title">
+                        <span>Text field</span>
+                    </div>
+                    <div class="sf-preview">
+                        <input type="text" placeholder="Dummy placeholder..." />
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-textarea">
+                    <div class="sf-title">
+                        <span>Textarea</span>
+                    </div>
+                    <div class="sf-preview">
+                        <textarea placeholder="Dummy placeholder..."></textarea>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-checkbox">
+                    <div class="sf-title">
+                        <span>Checkboxes</span>
+                    </div>
+                    <div class="sf-preview">
+                        <ul>
+                            <li>Option 1</li>
+                            <li>Option 2</li>
+                            <li>Option 3</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-radio">
+                    <div class="sf-title">
+                        <span>Radio buttons</span>
+                    </div>
+                    <div class="sf-preview">
+                        <ul>
+                            <li>Option 1</li>
+                            <li>Option 2</li>
+                            <li>Option 3</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="sf-item sf-type-dropdown">
+                    <div class="sf-title">
+                        <span>Dropdown</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-dropdown">- select an option -</div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-quantity">
+                    <div class="sf-title">
+                        <span>Quantity</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-quantity">
+                            <span class="sf-min"></span>
+                            <input type="text" value="1" />
+                            <span class="sf-plus"></span>
                         </div>
                     </div>
-                    <?php
-                    $i++;
-                }
-                ?>
+                </div>
+
+                <div class="sf-item sf-type-toggle">
+                    <div class="sf-title">
+                        <span>Toggle</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-toggle">Off</div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-color">
+                    <div class="sf-title">
+                        <span>Color picker</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-color"></div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-slider">
+                    <div class="sf-title">
+                        <span>Range slider</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-slider"></div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-currency">
+                    <div class="sf-title">
+                        <span>Currency field</span>
+                    </div>
+                    <div class="sf-preview">
+                        <input type="text" value="$12,345.95" />
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-file">
+                    <div class="sf-title">
+                        <span>File upload</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-file">Upload files...</div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-date">
+                    <div class="sf-title">
+                        <span>Date picker</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-date">01-01-2018</div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-time">
+                    <div class="sf-title">
+                        <span>Time picker</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-time">16:45</div>
+                    </div>
+                </div>
+
+                <div class="sf-item sf-type-rating">
+                    <div class="sf-title">
+                        <span>Rating</span>
+                    </div>
+                    <div class="sf-preview">
+                        <div class="sf-rating">
+                            <i></i>
+                            <i></i>
+                            <i></i>
+                            <i></i>
+                            <i></i>
+                        </div>
+                    </div>
+                </div>
+
+
+               
             </div>
         </div>
 
