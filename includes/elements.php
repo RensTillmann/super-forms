cca8374b127ee8acfd402e7848c3b5aa5bc477b7
Rens <feeling4design@gmail.com>
2019-01-07 10:40:01 +0100

    context menu on navigator items
    progress edit panel settings

--- includes/elements.php
+++ includes/elements.php
@@ -74,146 +74,151 @@ return array(
                 'title' => __( 'Placeholder', 'super-forms' ),
                 'type' => 'text',
                 'default' => __( 'Dummy placeholder...', 'super-forms' ),
-                'placeholder' => __( 'Enter a placeholder', 'super-forms' )
+                'placeholder' => __( 'Enter a placeholder', 'super-forms' ),
+                'selector' => '.sf-field input',
+                'property' => 'placeholder'
             ),
             'label' => array(
                 'title' => __( 'Field label', 'super-forms' ),
-                'options' => array(
-                    'value' => array(
-                        'type' => 'text',
-                        'default' => __( 'Dummy label', 'super-forms' ),
-                        'placeholder' => __( 'Enter a field label', 'super-forms' )
-                    ),
+                'type' => 'text',
+                'default' => __( 'Dummy label', 'super-forms' ),
+                'placeholder' => __( 'Enter a field label', 'super-forms' ),
+                'selector' => '.sf-label',
+                'property' => 'innerHTML',
+                'sub_options' => array(
                     'font' => array(
-                        'color' => array(
-                            'type' => 'color_picker',
-                            'title' => __( 'Color', 'super-forms' ),
-                            'default' => '#000',
-                            'selector' => '.super-label',
-                            'style_update' => 'color'
-                        ),
-                        'family' => array(
-                            'type' => 'dropdown;font',
-                            'title' => __( 'Family', 'super-forms' ),
-                            'default' => 'Default',
-                            'selector' => '.super-label',
-                            'style_update' => 'fontFamily'
-                        ),
-                        'size' => array(
-                            'type' => 'slider',
-                            'title' => __( 'Size', 'super-forms' ),
-                            'min' => 1,
-                            'max' => 50,
-                            'step' => 1,
-                            'units' => array(
-                                'px', 
-                                'em', 
-                                'rem'
+                        'title' => __( 'Font settings', 'super-forms' ),
+                        'options' => array(
+                            'color' => array(
+                                'type' => 'color_picker',
+                                'title' => __( 'Color', 'super-forms' ),
+                                'default' => '#000',
+                                'selector' => '.super-label',
+                                'style_update' => 'color'
                             ),
-                            'default' => array(
-                                'value' => '12',
-                                'unit' => 'px'
+                            'family' => array(
+                                'type' => 'dropdown;font',
+                                'title' => __( 'Family', 'super-forms' ),
+                                'default' => 'Default',
+                                'selector' => '.super-label',
+                                'style_update' => 'fontFamily'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'fontSize'
-                        ),
-                        'weight' => array(
-                            'type' => 'dropdown',
-                            'title' => __( 'Weight', 'super-forms' ),
-                            'default' => 'default',
-                            'options' => array(
-                                '100' => '100',
-                                '200' => '200',
-                                '300' => '300',
-                                '400' => '400',
-                                '500' => '500',
-                                '600' => '600',
-                                '700' => '700',
-                                '800' => '800',
-                                '900' => '900',
-                                'default' => __( 'Default', 'super-forms' ),
-                                'normal' => __( 'Normal', 'super-forms' ),
-                                'bold' => __( 'Bold', 'super-forms' )
+                            'size' => array(
+                                'type' => 'slider',
+                                'title' => __( 'Size', 'super-forms' ),
+                                'min' => 1,
+                                'max' => 50,
+                                'step' => 1,
+                                'units' => array(
+                                    'px', 
+                                    'em', 
+                                    'rem'
+                                ),
+                                'default' => array(
+                                    'value' => '12',
+                                    'unit' => 'px'
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'fontSize'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'fontWeight'
-                        ),
-                        'transform' => array(
-                            'type' => 'dropdown',
-                            'title' => __( 'Transform', 'super-forms' ),
-                            'default' => 'default',
-                            'options' => array(
-                                'default' => __( 'Default', 'super-forms' ),
-                                'uppercase' => __( 'Uppercase', 'super-forms' ),
-                                'lowercase' => __( 'Lowercase', 'super-forms' ),
-                                'capitalize' => __( 'Capitalize', 'super-forms' ),
-                                'normal' => __( 'Normal', 'super-forms' )
+                            'weight' => array(
+                                'type' => 'dropdown',
+                                'title' => __( 'Weight', 'super-forms' ),
+                                'default' => 'default',
+                                'options' => array(
+                                    '100' => '100',
+                                    '200' => '200',
+                                    '300' => '300',
+                                    '400' => '400',
+                                    '500' => '500',
+                                    '600' => '600',
+                                    '700' => '700',
+                                    '800' => '800',
+                                    '900' => '900',
+                                    'default' => __( 'Default', 'super-forms' ),
+                                    'normal' => __( 'Normal', 'super-forms' ),
+                                    'bold' => __( 'Bold', 'super-forms' )
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'fontWeight'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'textTransform'
-                        ),
-                        'style' => array(
-                            'type' => 'dropdown',
-                            'title' => __( 'Style', 'super-forms' ),
-                            'default' => 'default',
-                            'options' => array(
-                                'default' => __( 'Default', 'super-forms' ),
-                                'normal' => __( 'Normal', 'super-forms' ),
-                                'italic' => __( 'Italic', 'super-forms' ),
-                                'oblique' => __( 'Oblique', 'super-forms' )
+                            'transform' => array(
+                                'type' => 'dropdown',
+                                'title' => __( 'Transform', 'super-forms' ),
+                                'default' => 'default',
+                                'options' => array(
+                                    'default' => __( 'Default', 'super-forms' ),
+                                    'uppercase' => __( 'Uppercase', 'super-forms' ),
+                                    'lowercase' => __( 'Lowercase', 'super-forms' ),
+                                    'capitalize' => __( 'Capitalize', 'super-forms' ),
+                                    'normal' => __( 'Normal', 'super-forms' )
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'textTransform'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'fontStyle'
-                        ),
-                        'decoration' => array(
-                            'type' => 'dropdown',
-                            'title' => __( 'Decoration', 'super-forms' ),
-                            'default' => 'default',
-                            'options' => array(
-                                'default' => __( 'Default', 'super-forms' ),
-                                'underline' => __( 'Underline', 'super-forms' ),
-                                'overline' => __( 'Overline', 'super-forms' ),
-                                'line-through' => __( 'Line Through', 'super-forms' ),
-                                'none' => __( 'None', 'super-forms' )
+                            'style' => array(
+                                'type' => 'dropdown',
+                                'title' => __( 'Style', 'super-forms' ),
+                                'default' => 'default',
+                                'options' => array(
+                                    'default' => __( 'Default', 'super-forms' ),
+                                    'normal' => __( 'Normal', 'super-forms' ),
+                                    'italic' => __( 'Italic', 'super-forms' ),
+                                    'oblique' => __( 'Oblique', 'super-forms' )
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'fontStyle'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'textDecoration'
-                        ),
-                        'line_height' => array(
-                            'type' => 'slider',
-                            'title' => __( 'Line-height', 'super-forms' ),
-                            'min' => 1,
-                            'max' => 50,
-                            'step' => 1,
-                            'units' => array(
-                                'px', 
-                                'em', 
-                                'rem'
+                            'decoration' => array(
+                                'type' => 'dropdown',
+                                'title' => __( 'Decoration', 'super-forms' ),
+                                'default' => 'default',
+                                'options' => array(
+                                    'default' => __( 'Default', 'super-forms' ),
+                                    'underline' => __( 'Underline', 'super-forms' ),
+                                    'overline' => __( 'Overline', 'super-forms' ),
+                                    'line-through' => __( 'Line Through', 'super-forms' ),
+                                    'none' => __( 'None', 'super-forms' )
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'textDecoration'
                             ),
-                            'default' => array(
-                                'value' => '12',
-                                'unit' => 'px'
+                            'line_height' => array(
+                                'type' => 'slider',
+                                'title' => __( 'Line-height', 'super-forms' ),
+                                'min' => 1,
+                                'max' => 50,
+                                'step' => 1,
+                                'units' => array(
+                                    'px', 
+                                    'em', 
+                                    'rem'
+                                ),
+                                'default' => array(
+                                    'value' => '12',
+                                    'unit' => 'px'
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'lineHeight'
                             ),
-                            'selector' => '.super-label',
-                            'style_update' => 'lineHeight'
-                        ),
-                        'spacing' => array(
-                            'type' => 'slider',
-                            'title' =>  __( 'Letter spacing', 'super-forms' ),
-                            'min' => -5,
-                            'max' => 10,
-                            'step' => 0.1,
-                            'units' => array(
-                                'px', 
-                                'em', 
-                                'rem'
-                            ),
-                            'default' => array(
-                                'value' => '12',
-                                'unit' => 'px'
-                            ),
-                            'selector' => '.super-label',
-                            'style_update' => 'letterSpacing'
+                            'spacing' => array(
+                                'type' => 'slider',
+                                'title' =>  __( 'Letter spacing', 'super-forms' ),
+                                'min' => -5,
+                                'max' => 10,
+                                'step' => 0.1,
+                                'units' => array(
+                                    'px', 
+                                    'em', 
+                                    'rem'
+                                ),
+                                'default' => array(
+                                    'value' => '12',
+                                    'unit' => 'px'
+                                ),
+                                'selector' => '.super-label',
+                                'style_update' => 'letterSpacing'
+                            )
                         )
                     )
                 )
