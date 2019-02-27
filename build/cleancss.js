2c6ab2071a82bcde6ac940390b4a61a8252400f3
Rens <feeling4design@gmail.com>
2019-02-27 18:43:58 +0100

    change doc dir

--- build/cleancss.js
+++ build/cleancss.js
@@ -10,7 +10,6 @@ var options = {
   compatibility: 'ie9' 
 };
 var walkSync = function(dir, filelist) {
-  var needle = 'super-forms-build';
   var fs = fs || require('fs'),
   files = fs.readdirSync(dir);
   filelist = filelist || [];
@@ -19,9 +18,8 @@ var walkSync = function(dir, filelist) {
       filelist = walkSync(dir + file + '/', filelist);
     }else{
       if(path.extname(file)==='.css'){
-        var slice_position = dir.indexOf(needle);
         filelist.push({
-          path : dir.slice(slice_position+needle.length),
+          path : dir,
           name : file
         });
       }
@@ -31,8 +29,8 @@ var walkSync = function(dir, filelist) {
 };
 var files = walkSync(folder);
 files.forEach(function (file, index) {
-  var code = fs.readFileSync(ROOT+file.path+file.name, 'utf8');
+  var code = fs.readFileSync(file.path+file.name, 'utf8');
   var output = new CleanCSS(options).minify(code);
   console.log('Cleaning up '+file.name);
-  fs.writeFileSync(ROOT+file.path+file.name, output.styles, 'utf8');
+  fs.writeFileSync(file.path+file.name, output.styles, 'utf8');
 });
