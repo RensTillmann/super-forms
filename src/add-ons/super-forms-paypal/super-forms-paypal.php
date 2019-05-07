acd1a15ea1ea9532621066dc7c5d2829ecd7dcc8
Rens <feeling4design@gmail.com>
2019-05-07 21:50:18 +0200

    changelog

--- src/add-ons/super-forms-paypal/super-forms-paypal.php
+++ src/add-ons/super-forms-paypal/super-forms-paypal.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - PayPal Checkout
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Checkout with PayPal after form submission. Charge users for registering or posting content.
- * Version:     1.1.0
+ * Version:     1.1.1
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
  */
@@ -36,7 +36,7 @@ if (!class_exists('SUPER_PayPal')):
 		 *
 		 *  @since      1.0.0
 		 */
-		public $version = '1.1.0';
+		public $version = '1.1.1';
 
 		
 		/**
