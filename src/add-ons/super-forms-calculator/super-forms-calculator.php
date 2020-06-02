f72785ba45d333658e9096eeb05609f8acd34d5d
Rens <feeling4design@gmail.com>
2020-06-02 17:05:16 +0200

    calculator align center RTL fix

--- src/add-ons/super-forms-calculator/super-forms-calculator.php
+++ src/add-ons/super-forms-calculator/super-forms-calculator.php
@@ -11,7 +11,7 @@
  * Plugin Name: Super Forms - Calculator
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Adds an extra element that allows you to do calculations on any of your fields
- * Version:     2.1.4
+ * Version:     2.1.5
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
  * Text Domain: super-forms
@@ -39,7 +39,7 @@ if(!class_exists('SUPER_Calculator')) :
          *
          *	@since		1.0.0
         */
-        public $version = '2.1.4';
+        public $version = '2.1.5';
 
 
         /**
