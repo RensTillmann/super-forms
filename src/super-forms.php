0c513cd31ca2d597a80b54127b807eada28354ae
Rens <feeling4design@gmail.com>
2020-12-10 15:35:56 +0100

    address autocomplete option to filter by country and type

--- src/super-forms.php
+++ src/super-forms.php
@@ -14,7 +14,7 @@
  * Plugin Name: Super Forms - Drag & Drop Form Builder
  * Plugin URI:  http://codecanyon.net/user/feeling4design
  * Description: The most advanced, flexible and easy to use form builder for WordPress!
- * Version:     4.9.556
+ * Version:     4.9.557
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
  * Text Domain: super-forms
@@ -41,7 +41,7 @@ if(!class_exists('SUPER_Forms')) :
          *
          *  @since      1.0.0
         */
-        public $version = '4.9.556';
+        public $version = '4.9.557';
         public $slug = 'super-forms';
         public $apiUrl = 'https://api.super-forms.com/';
         public $apiVersion = 'v1';
