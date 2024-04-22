cf6461323a984500092560d1aad64b4bd3f74e2b
Rens <feeling4design@gmail.com>
2024-04-22 22:39:34 +0200

    update Stripe library to v14.2.0

--- src/includes/extensions/stripe/stripe-php/lib/EphemeralKey.php
+++ src/includes/extensions/stripe/stripe-php/lib/EphemeralKey.php
@@ -1,19 +1,16 @@
 <?php
 
+// File generated from our OpenAPI spec
+
 namespace Stripe;
 
 /**
- * Class EphemeralKey
- *
- * @property string $id
- * @property string $object
- * @property int $created
- * @property int $expires
- * @property bool $livemode
- * @property string $secret
- * @property array $associated_objects
- *
- * @package Stripe
+ * @property string $id Unique identifier for the object.
+ * @property string $object String representing the object's type. Objects of the same type share the same value.
+ * @property int $created Time at which the object was created. Measured in seconds since the Unix epoch.
+ * @property int $expires Time at which the key will expire. Measured in seconds since the Unix epoch.
+ * @property bool $livemode Has the value <code>true</code> if the object exists in live mode or the value <code>false</code> if the object exists in test mode.
+ * @property null|string $secret The key's secret. You can use this value to make authorized requests to the Stripe API.
  */
 class EphemeralKey extends ApiResource
 {
@@ -22,22 +19,24 @@ class EphemeralKey extends ApiResource
     use ApiOperations\Create {
         create as protected _create;
     }
+
     use ApiOperations\Delete;
 
     /**
-     * @param array|null $params
-     * @param array|string|null $opts
+     * @param null|array $params
+     * @param null|array|string $opts
      *
      * @throws \Stripe\Exception\InvalidArgumentException if stripe_version is missing
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
-     * @return EphemeralKey The created key.
+     * @return \Stripe\EphemeralKey the created key
      */
     public static function create($params = null, $opts = null)
     {
         if (!$opts || !isset($opts['stripe_version'])) {
             throw new Exception\InvalidArgumentException('stripe_version must be specified to create an ephemeral key');
         }
+
         return self::_create($params, $opts);
     }
 }
