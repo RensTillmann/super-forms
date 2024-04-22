55319edce75665163eb62fa7d76e00921b458eef
Rens <feeling4design@gmail.com>
2024-04-22 22:39:34 +0200

    update Stripe library to v14.2.0

--- src/includes/extensions/stripe/stripe-php/lib/ApiOperations/All.php
+++ src/includes/extensions/stripe/stripe-php/lib/ApiOperations/All.php
@@ -10,8 +10,8 @@ namespace Stripe\ApiOperations;
 trait All
 {
     /**
-     * @param array|null $params
-     * @param array|string|null $opts
+     * @param null|array $params
+     * @param null|array|string $opts
      *
      * @throws \Stripe\Exception\ApiErrorException if the request fails
      *
@@ -19,18 +19,8 @@ trait All
      */
     public static function all($params = null, $opts = null)
     {
-        self::_validateParams($params);
         $url = static::classUrl();
 
-        list($response, $opts) = static::_staticRequest('get', $url, $params, $opts);
-        $obj = \Stripe\Util\Util::convertToStripeObject($response->json, $opts);
-        if (!($obj instanceof \Stripe\Collection)) {
-            throw new \Stripe\Exception\UnexpectedValueException(
-                'Expected type ' . \Stripe\Collection::class . ', got "' . get_class($obj) . '" instead.'
-            );
-        }
-        $obj->setLastResponse($response);
-        $obj->setFilters($params);
-        return $obj;
+        return static::_requestPage($url, \Stripe\Collection::class, $params, $opts);
     }
 }
--- src/includes/extensions/stripe/stripe-php/lib/RequestTelemetry.php
+++ src/includes/extensions/stripe/stripe-php/lib/RequestTelemetry.php
@@ -3,25 +3,30 @@
 namespace Stripe;
 
 /**
- * Class RequestTelemetry
+ * Class RequestTelemetry.
  *
  * Tracks client request telemetry
- * @package Stripe
  */
 class RequestTelemetry
 {
+    /** @var string */
     public $requestId;
+    /** @var int */
     public $requestDuration;
+    /** @var string[] */
+    public $usage;
 
     /**
      * Initialize a new telemetry object.
      *
-     * @param string $requestId The request's request ID.
-     * @param int $requestDuration The request's duration in milliseconds.
+     * @param string $requestId the request's request ID
+     * @param int $requestDuration the request's duration in milliseconds
+     * @param string[] $usage names of tracked behaviors associated with this request
      */
-    public function __construct($requestId, $requestDuration)
+    public function __construct($requestId, $requestDuration, $usage = [])
     {
         $this->requestId = $requestId;
         $this->requestDuration = $requestDuration;
+        $this->usage = $usage;
     }
 }
